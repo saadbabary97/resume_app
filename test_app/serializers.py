a8cb9b00d49df1f7f0c80bc1bc9dd1e6729b770e
Saad Babary <saadbabary97@gmail.com>
2023-10-31 16:33:25 +0500

    commented code remove

--- test_app/serializers.py
+++ test_app/serializers.py
@@ -30,25 +30,3 @@ class PortfolioSerializer(serializers.ModelSerializer):
     class Meta:
         model = Portfolio
         fields = '__all__'
-
-    # def create(self, validated_data):
-    #     educations_data = validated_data.pop('educations')
-    #     work_experiences_data = validated_data.pop('work_experiences')
-    #     skills_data = validated_data.pop('skills')
-    #     hobbies_data = validated_data.pop('hobbies')
-
-    #     portfolio = Portfolio.objects.create(**validated_data)
-
-    #     for education_data in educations_data:
-    #         Education.objects.create(portfolio=portfolio, **education_data)
-
-    #     for work_experience_data in work_experiences_data:
-    #         WorkExperience.objects.create(portfolio=portfolio, **work_experience_data)
-
-    #     for skill_data in skills_data:
-    #         Skill.objects.create(portfolio=portfolio, **skill_data)
-
-    #     for hobby_data in hobbies_data:
-    #         Hobby.objects.create(portfolio=portfolio, **hobby_data)
-
-    #     return portfolio
